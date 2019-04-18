5fc81821cef179ecc36459c29353faab6b2c6dcc
Kijana Garrett <kjgarrett@d-p-13-180.eecs.tufts.edu>
2019-04-17 22:30:54 -0400

    Stage [3] – Placing removing func calls & assert

--- um.c
+++ um.c
@@ -32,21 +32,10 @@
 
 typedef enum UM_opcode
 {
-    CMOV = 0,
-    SLOAD,
-    SSTORE,
-    ADD,
-    MUL,
-    DIV,
-    NAND,
-    HALT,
-    MAP,
-    UNMAP,
-    OUTPUT,
-    INPUT,
-    LOADP,
-    LOADV
+    CMOV = 0, SLOAD, SSTORE, ADD, MUL, DIV, NAND, HALT, MAP, UNMAP,
+    OUTPUT, INPUT, LOADP, LOADV
 } UM_opcode;
+
 typedef struct UM
 {
     uint32_t prog_counter;
@@ -67,17 +56,15 @@ void execute(UM UM, SegMem SegMem);
 void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
                   uint32_t *prog_counter);
 void get_input(uint32_t *regC);
-void load_value(UM UM, uint32_t instruction);
-uint32_t map_seg_mem(SegMem SegMem, uint32_t size);
-void unmap_seg(SegMem SegMem, uint32_t location);
+// void load_value(UM UM, uint32_t instruction);
+// void unmap_seg(SegMem SegMem, uint32_t location);
 void free_all(UM UM, SegMem SegMem);
 UArray_T create_segment(int length);
 void seg_mem_free(SegMem SegMem);
-uint32_t map_seg_mem(SegMem SegMem, uint32_t size);
-void unmap_seg(SegMem SegMem, uint32_t location);
-void load_program(Seq_T seg_mem, uint32_t val);
-void sstore(Seq_T seg_mem, uint32_t regA, uint32_t regB, uint32_t regC);
-uint32_t sload(Seq_T seg_mem, uint32_t regB, uint32_t regC);
+// uint32_t map_seg_mem(SegMem SegMem, uint32_t size);
+// void load_program(Seq_T seg_mem, uint32_t val);
+// void sstore(Seq_T seg_mem, uint32_t regA, uint32_t regB, uint32_t regC);
+// uint32_t sload(Seq_T seg_mem, uint32_t regB, uint32_t regC);
 
 int main(int argc, char *argv[])
 {
@@ -112,7 +99,6 @@ int get_size(char *input)
         fprintf(stderr, "Incorrect File Size.\n");
         return EXIT_FAILURE;
     }
-    assert(st.st_size % 4 == 0);
     return st.st_size / 4;
 }
 
@@ -121,7 +107,6 @@ int get_size(char *input)
 UM create_UM()
 {
     UM UM = malloc(sizeof(struct UM));
-    assert(UM);
     UM->prog_counter = 0;
     UM->registers = malloc(NUM_REG * sizeof(uint32_t));
     for (int i = 0; i < NUM_REG; i++)
@@ -186,7 +171,9 @@ void execute(UM UM, SegMem SegMem)
     while (true)
     {
         // uint32_t curr_instruct = *(uint32_t *) UArray_at((UArray_T)Seq_get(SegMem->seg_mem, SEG_ZERO), UM->prog_counter);
-        run_instruct(UM, SegMem, *(uint32_t *)UArray_at((UArray_T)Seq_get(SegMem->seg_mem, SEG_ZERO), UM->prog_counter),
+        run_instruct(UM, SegMem, 
+        *(uint32_t *)UArray_at((UArray_T)Seq_get(SegMem->seg_mem, SEG_ZERO), 
+        UM->prog_counter),
                      &UM->prog_counter);
         UM->prog_counter++;
     }
@@ -204,51 +191,34 @@ void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
     uint32_t *regB = NULL;
     uint32_t *regC = NULL;
     if (opcode <= 6) {
+        A = (uint32_t)Bitpack_getu(instruction, ID_LEN, A_LSB);
+        B = (uint32_t)Bitpack_getu(instruction, ID_LEN, B_LSB);
+        C = (uint32_t)Bitpack_getu(instruction, ID_LEN, C_LSB);
         if (opcode <= 3) {
             if (opcode <= 1) {
                 if (opcode == 0) {
-                    A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               A_LSB);
-                    B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               B_LSB);
-                    C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               C_LSB);
                     regA = &((UM->registers)[A]);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
                     if (*regC != 0)
                         *regA = *regB;
                 } else {
-                    A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               A_LSB);
-                    B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               B_LSB);
-                    C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               C_LSB);
                     regA = &((UM->registers)[A]);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
-                    *regA = sload(SegMem->seg_mem, *regB, *regC);
+                    UArray_T curr_seg = Seq_get(SegMem->seg_mem, *regB);
+                    *regA = *((uint32_t *)UArray_at(curr_seg, *regC));
                 }
             } else {
                 if (opcode == 2) {
-                    A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               A_LSB);
-                    B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               B_LSB);
-                    C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               C_LSB);
                     regA = &((UM->registers)[A]);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
-                    sstore(SegMem->seg_mem, *regA, *regB, *regC);
+                    uint32_t *curr_offset;
+                    UArray_T curr_seg = Seq_get(SegMem->seg_mem, *regA);
+                    curr_offset = (uint32_t *)UArray_at(curr_seg, *regB);
+                    *curr_offset = *regC;
                 } else {
-                    A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               A_LSB);
-                    B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               B_LSB);
-                    C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               C_LSB);
                     regA = &((UM->registers)[A]);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
@@ -258,36 +228,17 @@ void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
         } else {
             if (opcode <= 5) {
                 if (opcode == 4) {
-                    A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               A_LSB);
-                    B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               B_LSB);
-                    C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               C_LSB);
                     regA = &((UM->registers)[A]);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
                     *regA = (*regB * *regC);
                 } else {
-                    A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               A_LSB);
-                    B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               B_LSB);
-                    C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                               C_LSB);
                     regA = &((UM->registers)[A]);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
-                    assert(*regC != 0);
                     *regA = (*regB / *regC);
                 }
             } else {
-                A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                           A_LSB);
-                B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                           B_LSB);
-                C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                           C_LSB);
                 regA = &((UM->registers)[A]);
                 regB = &((UM->registers)[B]);
                 regC = &((UM->registers)[C]);
@@ -305,14 +256,27 @@ void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
                     C = (uint32_t)Bitpack_getu(instruction, ID_LEN, C_LSB);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
-                    *regB = map_seg_mem(SegMem, *regC);
+                    UArray_T mapped_seg = create_segment(*regC);
+                    if (Seq_length(SegMem->unmapped_segs) > 0) {
+                        uint32_t location = (uint32_t)(uintptr_t)
+                            Seq_remlo(SegMem->unmapped_segs);
+                        Seq_put(SegMem->seg_mem, location, mapped_seg);
+                        *regB = location;
+                    } else {
+                        Seq_addhi(SegMem->seg_mem, mapped_seg);
+                        *regB = (Seq_length(SegMem->seg_mem) - 1);
+                    }
                 }
             }
             else {
                 if (opcode == 9) {
                     C = (uint32_t)Bitpack_getu(instruction, ID_LEN, C_LSB);
                     regC = &((UM->registers)[C]);
-                    unmap_seg(SegMem, *regC);
+                    // unmap_seg(SegMem, *regC);
+                    UArray_T unmap_seg = Seq_get(SegMem->seg_mem, *regC);
+                    UArray_free(&unmap_seg);
+                    Seq_put(SegMem->seg_mem, *regC, NULL);
+                    Seq_addhi(SegMem->unmapped_segs, (void *)(uintptr_t)*regC);
                 } else { //10
                     C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
                                                C_LSB);
@@ -326,7 +290,12 @@ void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
                     C = (uint32_t)Bitpack_getu(instruction, ID_LEN,
                                                C_LSB);
                     regC = &((UM->registers)[C]);
-                    get_input(regC);
+                    // get_input(regC);
+                    char input = getchar();
+                    if (input == EOF)
+                        *regC = ~0;
+                    else
+                        *regC = (uint32_t)input;
                 } else { //12
                     B = (uint32_t)Bitpack_getu(instruction, ID_LEN,
                                                B_LSB);
@@ -334,12 +303,32 @@ void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
                                                C_LSB);
                     regB = &((UM->registers)[B]);
                     regC = &((UM->registers)[C]);
-                    if (*regB != 0)
-                        load_program(SegMem->seg_mem, *regB);
+                    if (*regB != 0) {
+                            // load_program(SegMem->seg_mem, *regB);
+                            uint32_t *curr_elem;
+                            uint32_t *new_elem;
+                            UArray_T curr_seg = Seq_get(SegMem->seg_mem, *regB);
+                            int length = UArray_length(curr_seg);
+                            UArray_T new_seg = UArray_new(length, sizeof(uint32_t));
+                            for (int i = 0; i < length; i++) {
+                                curr_elem = (uint32_t *)UArray_at(curr_seg, i);
+                                new_elem = (uint32_t *)UArray_at(new_seg, i);
+                                *new_elem = *curr_elem;
+                            }
+                            UArray_T seg_zero = Seq_get(SegMem->seg_mem, SEG_ZERO);
+                            UArray_free(&seg_zero);
+                            Seq_put(SegMem->seg_mem, SEG_ZERO, NULL);
+                            Seq_put(SegMem->seg_mem, SEG_ZERO, new_seg);
+                    }
                     *prog_counter = (*regC - 1);
                 }
             } else { //13
-                load_value(UM, instruction);
+                // load_value(UM, instruction);
+                    uint32_t A = (uint32_t)Bitpack_getu(instruction, ID_LEN, 25);
+                    uint32_t value = (uint32_t)Bitpack_getu(instruction,
+                                                            VAL_LENGTH, VAL_LSB);
+                    uint32_t *regA = &((UM->registers)[A]);
+                    *regA = value;
             }
         }
     }
@@ -347,25 +336,25 @@ void run_instruct(UM UM, SegMem SegMem, uint32_t instruction,
 
 /* get_input: If the opcode is 11, this function is called and 
    applies an input value into register C */
-void get_input(uint32_t *regC)
-{
-    char input = getchar();
-    if (input == EOF)
-        *regC = ~0;
-    else
-        *regC = (uint32_t)input;
-}
+// void get_input(uint32_t *regC)
+// {
+//     char input = getchar();
+//     if (input == EOF)
+//         *regC = ~0;
+//     else
+//         *regC = (uint32_t)input;
+// }
 /* load_value: If the opcode is 13, this function is called and 
    loads a 25 bit value into register A*/
-void load_value(UM UM, uint32_t instruction)
-{
-    uint32_t A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
-                                        OP_LSB - ID_LEN);
-    uint32_t value = (uint32_t)Bitpack_getu(instruction,
-                                            VAL_LENGTH, VAL_LSB);
-    uint32_t *regA = &((UM->registers)[A]);
-    *regA = value;
-}
+// void load_value(UM UM, uint32_t instruction)
+// {
+//     uint32_t A = (uint32_t)Bitpack_getu(instruction, ID_LEN,
+//                                         OP_LSB - ID_LEN);
+//     uint32_t value = (uint32_t)Bitpack_getu(instruction,
+//                                             VAL_LENGTH, VAL_LSB);
+//     uint32_t *regA = &((UM->registers)[A]);
+//     *regA = value;
+// }
 
 /* create_segment: Initializes the first segment of 32 bit size based 
    on the length of the number of instructions given to the UM*/
@@ -398,70 +387,70 @@ void seg_mem_free(SegMem SegMem)
 
 /* Gets array at given Segment ID and returns the value at the given offset
    of the segment ID. */
-uint32_t sload(Seq_T seg_mem, uint32_t regB, uint32_t regC)
-{
-    UArray_T curr_seg = Seq_get(seg_mem, regB);
-    return *((uint32_t *)UArray_at(curr_seg, regC));
-}
+// uint32_t sload(Seq_T seg_mem, uint32_t regB, uint32_t regC)
+// {
+//     UArray_T curr_seg = Seq_get(seg_mem, regB);
+//     return *((uint32_t *)UArray_at(curr_seg, regC));
+// }
 
 /* Takes value at given register and places it in the a given memory segment 
    at a given offset. */
-void sstore(Seq_T seg_mem, uint32_t regA, uint32_t regB, uint32_t regC)
-{
-    uint32_t *curr_offset;
-    UArray_T curr_seg = Seq_get(seg_mem, regA);
-    curr_offset = (uint32_t *)UArray_at(curr_seg, regB);
-    *curr_offset = regC;
-}
+// void sstore(Seq_T seg_mem, uint32_t regA, uint32_t regB, uint32_t regC)
+// {
+//     uint32_t *curr_offset;
+//     UArray_T curr_seg = Seq_get(seg_mem, regA);
+//     curr_offset = (uint32_t *)UArray_at(curr_seg, regB);
+//     *curr_offset = regC;
+// }
 
 /* map_seg_mem: Based on if the opcode is 8, maps a new segment based 
   on the number of words in register C and makes an equivalently 
   segment in register B */
-uint32_t map_seg_mem(SegMem SegMem, uint32_t size)
-{
-    UArray_T mapped_seg = create_segment(size);
-    if (Seq_length(SegMem->unmapped_segs) > 0)
-    {
-        uint32_t location = (uint32_t)(uintptr_t)
-            Seq_remlo(SegMem->unmapped_segs);
-        Seq_put(SegMem->seg_mem, location, mapped_seg);
-        return location;
-    }
-    else
-    {
-        Seq_addhi(SegMem->seg_mem, mapped_seg);
-        return Seq_length(SegMem->seg_mem) - 1;
-    }
-}
+// uint32_t map_seg_mem(SegMem SegMem, uint32_t size)
+// {
+//     UArray_T mapped_seg = create_segment(size);
+//     if (Seq_length(SegMem->unmapped_segs) > 0)
+//     {
+//         uint32_t location = (uint32_t)(uintptr_t)
+//             Seq_remlo(SegMem->unmapped_segs);
+//         Seq_put(SegMem->seg_mem, location, mapped_seg);
+//         return location;
+//     }
+//     else
+//     {
+//         Seq_addhi(SegMem->seg_mem, mapped_seg);
+//         return Seq_length(SegMem->seg_mem) - 1;
+//     }
+// }
 
 /* unmap_seg: Based on if the opcode is 9, unmaps a segment at register
    C and then takes its ID so that it could be used again for future map calls.
     */
-void unmap_seg(SegMem SegMem, uint32_t location)
-{
-    UArray_T unmap_seg = Seq_get(SegMem->seg_mem, location);
-    UArray_free(&unmap_seg);
-    Seq_put(SegMem->seg_mem, location, NULL);
-    Seq_addhi(SegMem->unmapped_segs, (void *)(uintptr_t)location);
-}
+// void unmap_seg(SegMem SegMem, uint32_t location)
+// {
+//     UArray_T unmap_seg = Seq_get(SegMem->seg_mem, location);
+//     UArray_free(&unmap_seg);
+//     Seq_put(SegMem->seg_mem, location, NULL);
+//     Seq_addhi(SegMem->unmapped_segs, (void *)(uintptr_t)location);
+// }
 
 /* Load_program: Duplicates the segment connected to regB and puts it
    into the segment at ID 0 */
-void load_program(Seq_T seg_mem, uint32_t val)
-{
-    uint32_t *curr_elem;
-    uint32_t *new_elem;
-    UArray_T curr_seg = Seq_get(seg_mem, val);
-    int length = UArray_length(curr_seg);
-    UArray_T new_seg = UArray_new(length, sizeof(uint32_t));
-    for (int i = 0; i < length; i++)
-    {
-        curr_elem = (uint32_t *)UArray_at(curr_seg, i);
-        new_elem = (uint32_t *)UArray_at(new_seg, i);
-        *new_elem = *curr_elem;
-    }
-    UArray_T seg_zero = Seq_get(seg_mem, SEG_ZERO);
-    UArray_free(&seg_zero);
-    Seq_put(seg_mem, SEG_ZERO, NULL);
-    Seq_put(seg_mem, SEG_ZERO, new_seg);
-}
+// void load_program(Seq_T seg_mem, uint32_t val)
+// {
+//     uint32_t *curr_elem;
+//     uint32_t *new_elem;
+//     UArray_T curr_seg = Seq_get(seg_mem, val);
+//     int length = UArray_length(curr_seg);
+//     UArray_T new_seg = UArray_new(length, sizeof(uint32_t));
+//     for (int i = 0; i < length; i++)
+//     {
+//         curr_elem = (uint32_t *)UArray_at(curr_seg, i);
+//         new_elem = (uint32_t *)UArray_at(new_seg, i);
+//         *new_elem = *curr_elem;
+//     }
+//     UArray_T seg_zero = Seq_get(seg_mem, SEG_ZERO);
+//     UArray_free(&seg_zero);
+//     Seq_put(seg_mem, SEG_ZERO, NULL);
+//     Seq_put(seg_mem, SEG_ZERO, new_seg);
+// }
